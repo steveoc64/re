e762ee999cf4930ba373c32c19f330b451639d0f
steveo <steveo@benon.com>
2020-01-16 15:40:21 +1000

    extend TODO a bit

--- doc/todo.go
+++ doc/todo.go
@@ -9,4 +9,29 @@ dataLists
 	- Morale State
 
 
+UI - ranged
+
+	- Grande Skirmish
+	- Tactical Skirmish
+	- Bombardment Fire
+	- Tactical Gunnery
+
+UI - close action
+
+	- Infantry assault v
+		- Just guns
+		- Just SK
+		- Infantry / guns
+		- With cav support
+		- Defender in Towns
+
+	- SK assault
+		- Just guns
+		- Just SK
+		- Defender in Towns
+
+	- Cav Charge v
+		- Just guns
+		- Infantry +
+		- Cavalry
  */
